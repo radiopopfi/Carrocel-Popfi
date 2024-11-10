97ab3941885eb594c4dbc7a4333e8a1f56122151
radiopopfi <radiopopfi@gmail.com>
2024-11-10 11:51:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-/**
- * Creator           : Coding's Time
- * Youtube Channel   : https://www.youtube.com/channel/UC6dnKqrImGWqMb9ty1n0Ziw
- * Github Profile    : https://github.com/codingstime
- */
-
 let onSlide = false;
 
 window.addEventListener("load", () => {
@@ -37,28 +31,24 @@ function slide(toIndex) {
    let newItemActive = null;
 
    if (toIndex > itemActiveIndex) {
-      // check if toIndex exceeds the number of carousel items
       if (toIndex >= itemsArray.length) {
          toIndex = 0;
       }
 
       newItemActive = itemsArray[toIndex];
 
-      // start transition
       newItemActive.classList.add("carousel_item__pos_next");
       setTimeout(() => {
          newItemActive.classList.add("carousel_item__next");
          itemActive.classList.add("carousel_item__next");
       }, 20);
    } else {
-      // check if toIndex exceeds the number of carousel items
       if (toIndex < 0) {
          toIndex = itemsArray.length - 1;
       }
 
       newItemActive = itemsArray[toIndex];
 
-      // start transition
       newItemActive.classList.add("carousel_item__pos_prev");
       setTimeout(() => {
          newItemActive.classList.add("carousel_item__prev");
@@ -66,7 +56,6 @@ function slide(toIndex) {
       }, 20);
    }
 
-   // remove all transition class and switch active class
    newItemActive.addEventListener("transitionend", () => {
       itemActive.className = "carousel_item";
       newItemActive.className = "carousel_item carousel_item__active";
@@ -81,8 +70,7 @@ function slide(toIndex) {
 function getItemActiveIndex() {
    const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
    const itemActive = document.querySelector(".carousel_item__active");
-   const itemActiveIndex = itemsArray.indexOf(itemActive);
-   return itemActiveIndex;
+   return itemsArray.indexOf(itemActive);
 }
 
 function slideIndicator(toIndex) {
